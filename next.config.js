e9d851568b82871ec5e49797c03043dcaf4af748
Brij-Paul Singh <brijpaulsingh@hotmail.com>
2025-04-06 19:23:39 -0400

    Fix Next.js font configuration and remove Babel

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000'],
     },
   },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 module.exports = nextConfig;
